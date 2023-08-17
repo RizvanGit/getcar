cbc2019d67ee3a3ab0948c560e17f21271a9e490
Rizvan <imeandmyep2@gmail.com>
2023-08-17 13:45:58 +0300

    feat: updating state by choosing manufacturer

--- components/Main/SearchBar/SearchBar.tsx
+++ components/Main/SearchBar/SearchBar.tsx
@@ -5,12 +5,16 @@ import { SearchManufacturer } from "@/components";
 
 const SearchBar: FC = () => {
   const [manufacturer, setManufacturer] = useState("");
+  console.log(`Manufacturer state: ${manufacturer}`);
 
   const submitHandler = () => { };
   return (
     <form className="searchbar" onSubmit={submitHandler}>
       <div className="searchbar__item">
-        <SearchManufacturer />
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
       </div>
     </form>
   );
